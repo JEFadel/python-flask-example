b4083e95ac391df603bba7f561d3c37ae92c8536
João Elias Fadel <j.elias5@me.com>
2022-10-28 00:00:08 +0000

    alteracao 3

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ def health():
     return str(os.environ.get("STATUS"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=(os.environ.get("APP_PORT")), debug=True)
 
     # sudo apt install python3-pip
     # pip install -r requirements.txt
     # Oi, profº, deixando esse comentário só pra não dizer que meu código ficou igual o de todo mundo :P
+    
+    # docker build . -t localhost:32000/app-flask:latest
+    # docker run --rm -d -e APP_PORT=3333
