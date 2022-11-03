cb9076888fc09ee5728fc2f7255d6fb637887c63
SEU NOME AQUI <seu@email.aqui>
2022-11-03 23:54:38 +0000

    Adicionar counter

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-
+count = 0
 
 @app.route("/")
 def hello_world():
@@ -11,6 +11,12 @@ def hello_world():
 @app.route("/health")
 def health():
     return "OK"
+
+@app.route("/counter")
+def counter():
+    global count
+    count += 1
+    return str(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=(os.environ.get("APP_PORT")), debug=True)
